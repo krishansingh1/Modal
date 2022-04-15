0579efc5f488a548afa9f301f67e1f980d58b522
krishansingh1 <kkrishan7074@gmail.com>
2022-04-15 13:45:30 +0530

    Commit

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const open= document.querySelector('.btn');
+const open = document.querySelector('.btn');
+const modal = document.querySelector('.modal-overlay');
+const close = document.querySelector('.close-btn');
 
-open.addEventListener('click',()=>{
-    
+open.addEventListener('click', () => {
+    console.log(modal.classList);
+    modal.classList.add('open-modal')
+})
+
+close.addEventListener('click', () => {
+  modal.classList.remove('open-modal')
 })
